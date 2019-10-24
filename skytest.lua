e2e9d1eb91eddd0ff51730bf69ba3f4fcbbe6850
sungeliang <sungelianginsky@163.com>
2019-10-24 16:48:55 +0700

    learn relate on the internet

--- skytest.lua
+++ skytest.lua
@@ -21,4 +21,30 @@ function getLuaTest()
     print("git reset --hard commit-id")
 
     print("git restore -- xx.x 回撤")
+    print("rm xxx.x in work directory")
+    print("git restore -- xx.x")
+    print("git rm xx.x")
+    print("git commit -m \"xxxxx\"")
+    print("$ ssh-keygen -t rsa -C \"youremail@example.com\"")
+
+--[[     …or create a new repository on the command line
+    echo "# learngit" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git remote add origin https://github.com/tonybeef/learngit.git
+    git push -u origin master
+    …or push an existing repository from the command line
+    git remote add origin https://github.com/tonybeef/learngit.git
+    git push -u origin master
+    …or import code from another repository
+    You can initialize this repository with code from a Subversion, Mercurial, or TFS project. ]]
+
+    --[[
+        1.本地版本库关联远程版本库，首先看下本地目录下有没有.ssh文件夹，没有的话就用命令行ssh-keygen -t rsa -c "sungelianginsky@163.com"创建一个.ssh文件夹
+        里面有公钥和私钥， id_rsa.pub是公钥
+        2.登录github把ssh的公钥复制添加到ssh钥匙里面去
+        3.创建一个版本库，本地输入命令行git remote add origin https://github.com/tonybeef/learngit.git关联远程版本库
+        4.git push -u origin master 把本地版本库更新到远程版本库里面去
+    ]]
 end
