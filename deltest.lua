1de717f1dc17f6f985d37dacbd10987dbbeefd71
sungeliang <sungelianginsky@163.com>
2019-10-24 15:31:38 +0700

    del test

---
+++ deltest.lua
@@ -0,0 +1,22 @@
+function getLuaTest()
+    print("your father is also your father")
+    print("C:/Users/skyline/Desktop/skytest.lua")
+    print("test-git fighting")
+    print("git init create a empty github")
+    print("git config --global user.name \'sungeliang\'
+            git config --global user.email \'sungelianginsky@163.com\'")
+    print("madir learngit
+           cd learngit
+           pwd")
+    print("git add xxx.xx")
+    print("git commit -m \'detail\'")
+    print("git status")
+    print("git diff xxx.x")
+
+    print("git log")
+    print("git reflog")
+    print("git log --pretty=oneline")
+    print("git reflog")
+    print("git reset --hard HEAD^")
+    print("git reset --hard commit-id")
+end
